fb12b52667e01d4b4112b33674424a933e45d07f
Abhik Patra <theabhik2020@gmail.com>
2024-10-09 01:00:17 +0530

    notes

--- app_1/src/app/auth/sign-in/page.tsx
+++ app_1/src/app/auth/sign-in/page.tsx
@@ -1,9 +1,21 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { useState } from "react";
+import { useSession, signIn, signOut, getProviders } from "next-auth/react";
+import { useState, useEffect } from "react";
+
 export default function Component() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [providers, setProviders] = useState<any>();
+  const { data: session,status  } = useSession();
+  console.log(status)
+  const fetchProviders = async () => {
+    const res = await getProviders();
+    setProviders(res);
+  };
+  useEffect(() => {
+    fetchProviders();
+  }, []);
+
   const handleLogin = () => {
     signIn("credentials", {
       email: email,
@@ -12,7 +24,7 @@ export default function Component() {
       callbackUrl: "/",
     });
   };
-  const { data: session } = useSession();
+
   if (session) {
     return (
       <>
@@ -21,25 +33,34 @@ export default function Component() {
       </>
     );
   }
+
   return (
     <>
       <div className="form">
         <input
-          className=" border border-slate-400 block my-3"
+          className="border border-slate-400 block my-3"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           type="text"
           name="email"
         />
         <input
-          className=" border border-slate-400 block my-3"
+          className="border border-slate-400 block my-3"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          type="text"
+          type="password"
           name="password"
         />
       </div>
       <button onClick={handleLogin}>Sign in</button>
+      {providers &&
+        Object.values(providers).map((provider:any) => (
+          <div key={provider?.name}>
+            <button
+              onClick={() => signIn(provider?.id)}
+            >{`Sign in with ${provider?.name}`}</button>
+          </div>
+        ))}
     </>
   );
 }
